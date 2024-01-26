1e7fa4f05687b77ac4626a33c020ef965c20fc37
dababycar1337 <157720797+dababycar1337@users.noreply.github.com>
2024-01-26 09:17:17 +0300

    Add files via upload

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main()
+        {
+            First fr = new First(int.Parse(Console.ReadLine()), bool.Parse(Console.ReadLine()));
+            fr.FindMiddle();
+            fr.OnlyLower();
+            fr.NoRepeat();
+            fr.First();
+        }
+    }
+}
